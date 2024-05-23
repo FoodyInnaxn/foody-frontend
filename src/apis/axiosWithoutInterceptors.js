541e82048a7d78e2bbdd5a7f739773db11e696aa
Ivana Nedelkova <i.nedelkova@student.fontys.nl>
2024-05-23 15:55:27 +0200

    test var

--- src/apis/axiosWithoutInterceptors.js
+++ src/apis/axiosWithoutInterceptors.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const axiosWithoutInterceptors= ()=> axios.create({baseURL: process.env.HOST_URL})
+export const axiosWithoutInterceptors= ()=> axios.create({baseURL: 'foody-api-gateway.default.svc.cluster.local'})
 
 
 export default axiosWithoutInterceptors
